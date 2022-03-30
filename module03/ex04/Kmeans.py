974af7819656c3488f51c3409de1e54b9fba419a
Glagan <8447621+Glagan@users.noreply.github.com>
2022-03-30 18:19:09 +0200

    Add plots in Kmeans clusters

--- module03/ex04/Kmeans.py
+++ module03/ex04/Kmeans.py
@@ -1,8 +1,12 @@
+import sys
 import numpy as np
+from csvreader import CsvReader
+import matplotlib.pyplot as plt
+import itertools
 
 
 class KmeansClustering:
-    def __init__(self, max_iter=20, ncentroid=4):
+    def __init__(self, ncentroid=4, max_iter=30):
         self.ncentroid = ncentroid  # number of centroids
         self.max_iter = max_iter  # number of max iterations to update the centroids
         self.centroids = []  # values of the centroids
@@ -18,14 +22,13 @@ class KmeansClustering:
         Raises:
             This function should not raise any Exception.
         """
-        self.centroids = X[np.random.choice(
-            X.shape[0], size=self.ncentroid, replace=False)]
+        self.centroids = X[np.random.choice(X.shape[0], size=self.ncentroid, replace=False)]
+        print("initial centroids {}".format(self.centroids))
         if X.shape[0] < self.ncentroid:
             return None
+        positions = []
         for i in range(self.max_iter):
-            clusters = list([] for i in range(self.ncentroid))
-            current_distances = []
-
+            clusters = [[] for _ in range(self.ncentroid)]
             for row in X:
                 nearest = False
                 for j, centroid in enumerate(self.centroids):
@@ -33,10 +36,16 @@ class KmeansClustering:
                     if not nearest or distance < nearest[1]:
                         nearest = (j, distance)
                 clusters[nearest[0]].append(row)
-
+            # Save positions history to get a line from start to finish for each clusters
+            current_positions = []
             for j, cluster in enumerate(clusters):
                 matrix = np.array(cluster)
                 self.centroids[j] = np.mean(matrix, axis=0)
+                current_positions.append(self.centroids[j].tolist())
+            positions.append(current_positions)
+            if i >= 1 and positions[i - 1] == positions[i]:
+                break
+        return np.array(positions)
 
     def predict(self, X):
         """
@@ -52,6 +61,8 @@ class KmeansClustering:
         Raises:
             This function should not raise any Exception.
         """
+        if self.ncentroid != 4:
+            return None
         res = []
         for row in X:
             nearest = False
@@ -60,5 +71,70 @@ class KmeansClustering:
                 if not nearest or distance < nearest[1]:
                     nearest = (j, distance, row)
             res.append(nearest[0])
-        # TODO: Missing which clusters belong to which Solar Citizen
         return np.array(res)
+
+    def match_clusters(self):
+        # TODO: Missing which clusters belong to which Solar Citizen
+        # TODO: Check the values for each clusters
+        # TODO: Dataset [height, weight, bone_density]
+        # TODO: Belt: biggest mean height + Lowest bone density
+        # TODO: Mars: second mean height
+        # TODO: Venus: Lowest mean weights of the remaining
+        # TODO: Earth: remaining cluster
+        pass
+
+
+if __name__ == '__main__':
+    argc = len(sys.argv)
+    if argc != 4:
+        print('Missing or too many arguments')
+        print("Usage: Kmeans.py filepath='solar_system_census.csv' ncentroid=4 max_iter=30")
+        exit()
+    dataset_path, ncentroid, max_iter = sys.argv[1:]
+    # Parse arguments
+    try:
+        ncentroid = int(ncentroid)
+        if ncentroid < 2:
+            raise ValueError("ncentroid should be >= 2")
+    except BaseException:
+        print("Invalid ncentroid `{}`".format(ncentroid))
+    try:
+        max_iter = int(max_iter)
+        if max_iter < 0:
+            raise ValueError("max_iter should be positive")
+    except BaseException:
+        print("Invalid max_iter `{}`".format(max_iter))
+    # Open dataset
+    dataset = False
+    with CsvReader(dataset_path) as file:
+        if file is None:
+            print('Failed to read dataset')
+            exit()
+        dataset = np.array(file.getdata(), dtype='float')
+    if dataset is False or len(dataset.shape) != 2 and dataset.shape[1] != 4:
+        print('Invalid dataset')
+        exit()
+    # Remove "ID" column
+    dataset = np.delete(dataset, 0, axis=1)
+    # Fit and show results !
+    kmeans = KmeansClustering(ncentroid=ncentroid, max_iter=max_iter)
+    history = kmeans.fit(dataset)
+    print("history", history)
+    predictions = kmeans.predict(dataset)
+    # Show the results
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    cmap = plt.cm.get_cmap('tab20c', ncentroid)
+    markers = ['+', ',', 'o', '^']
+    for i, value in enumerate(dataset):
+        z, x, y = value
+        ax.scatter3D(x, y, z, s=20, marker=markers[predictions[i] % 4], color=cmap(predictions[i]))
+    for i in range(ncentroid):
+        # Select the i-th row of the positions history
+        line = history[0:, i]
+        z, x, y = line[:, 0], line[:, 1], line[:, 2]
+        ax.plot3D(x, y, z, color=cmap(i))
+        last_z, last_x, last_y = z[-1], x[-1], y[-1]
+        print("({}) last is {}".format(i, last_z))
+        ax.scatter3D(last_x, last_y, last_z, s=60, marker=markers[predictions[i] % 4], color=cmap(i))
+    plt.show()
---
+++ module03/ex04/csvreader.py
@@ -0,0 +1,66 @@
+import os
+
+
+class CsvReader():
+    def __init__(self, filename=None, sep=',', header=True, skip_top=0, skip_bottom=0):
+        self.filename = filename
+        self.sep = sep
+        self.header = header
+        self.skip_top = skip_top
+        self.skip_bottom = skip_bottom
+
+    def __enter__(self):
+        self.file = None
+        if self.filename is None:
+            return None
+        if self.skip_bottom < 0 or self.skip_top < 0:
+            return None
+        try:
+            self.file = open(self.filename, 'r')
+        except OSError as error:
+            print('Error: {}'.format(error))
+            return None
+
+        header = []
+        data = []
+        columns = False
+        for line in self.file:
+            line_data = [column.strip('\n').strip()
+                         for column in line.split(self.sep)]
+            if not header:
+                header = line_data
+                if self.header:
+                    columns = len(header)
+            else:
+                line_data = [column for column in line_data if column]
+                if not columns:
+                    columns = len(line_data)
+                if len(line_data) != columns:
+                    print('Invalid number of columns for line (expected {}):\n{}'.format(columns, line))
+                    return None
+                data.append(line_data)
+        if not self.header:
+            data.insert(0, header)
+            header = None
+
+        class CsvFile:
+            def __init__(self, header, data):
+                self.header = header
+                self.data = data
+
+            def __iter__(self):
+                return data.__iter__()
+
+            def getdata(self):
+                return self.data
+
+            def getheader(self):
+                return self.header
+
+        if self.skip_bottom == 0:
+            self.skip_bottom = -len(data)
+        return CsvFile(header, data[self.skip_top:-self.skip_bottom])
+
+    def __exit__(self, exc_type, exc_value, exc_trace):
+        if self.file:
+            self.file.close()
