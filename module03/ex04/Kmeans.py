c2509a1a2cb1e3c508cbb948c723b0cd4da29a09
Glagan <8447621+Glagan@users.noreply.github.com>
2022-03-31 20:06:55 +0200

    Find planets corresponding to clusters in exercise 4 Add 3D plot to visualize clusters in exercise 4

--- module03/ex04/Kmeans.py
+++ module03/ex04/Kmeans.py
@@ -6,12 +6,16 @@ import itertools
 
 
 class KmeansClustering:
-    def __init__(self, ncentroid=4, max_iter=30):
+    def __init__(self, max_iter=30, ncentroid=4):
+        if not isinstance(ncentroid, int) or ncentroid < 1:
+            raise ValueError('ncentroid should be a positive number')
+        if not isinstance(max_iter, int) or max_iter < 1:
+            raise ValueError('max_iter should be a positive number')
         self.ncentroid = ncentroid  # number of centroids
         self.max_iter = max_iter  # number of max iterations to update the centroids
         self.centroids = []  # values of the centroids
 
-    def fit(self, X):
+    def fit(self, X: np.ndarray):
         """
         Run the K-means clustering algorithm.
         For the location of the initial centroids, random pick ncentroids from the dataset.
@@ -22,8 +26,11 @@ class KmeansClustering:
         Raises:
             This function should not raise any Exception.
         """
+        if not isinstance(X, np.ndarray):
+            return None
+        if len(X.shape) != 2:
+            return None
         self.centroids = X[np.random.choice(X.shape[0], size=self.ncentroid, replace=False)]
-        print("initial centroids {}".format(self.centroids))
         if X.shape[0] < self.ncentroid:
             return None
         positions = []
@@ -47,13 +54,14 @@ class KmeansClustering:
                 break
         return np.array(positions)
 
-    def predict(self, X):
+    def predict(self, X: np.ndarray):
         """
         Predict from wich cluster each datapoint belongs to.
         smallest weight -> Venus
         biggest height + smallest bone density -> Belt
         biggest height -> Mars
         remaining -> Earth
+        Dataset: height,weight,bone_density
         Args:
             X: has to be an numpy.ndarray, a matrice of dimension m * n.
         Returns:
@@ -61,27 +69,66 @@ class KmeansClustering:
         Raises:
             This function should not raise any Exception.
         """
-        if self.ncentroid != 4:
+        if not isinstance(X, np.ndarray):
             return None
-        res = []
+        if len(X.shape) != 2:
+            return None
+
+        # Map citizens to clusters
+        clusters_citizens = [[] for _ in range(self.ncentroid)]
+        citizens = []
         for row in X:
             nearest = False
             for j, centroid in enumerate(self.centroids):
                 distance = np.linalg.norm(centroid - row, 1)
                 if not nearest or distance < nearest[1]:
                     nearest = (j, distance, row)
-            res.append(nearest[0])
-        return np.array(res)
+            clusters_citizens[nearest[0]].append(row)
+            citizens.append(nearest[0])
+        citizens = np.array(citizens)
 
-    def match_clusters(self):
-        # TODO: Missing which clusters belong to which Solar Citizen
-        # TODO: Check the values for each clusters
-        # TODO: Dataset [height, weight, bone_density]
-        # TODO: Belt: biggest mean height + Lowest bone density
-        # TODO: Mars: second mean height
-        # TODO: Venus: Lowest mean weights of the remaining
-        # TODO: Earth: remaining cluster
-        pass
+        if self.ncentroid != 4:
+            return [citizens, []]
+
+        # Map clusters to planets
+        planets = dict()
+        # Calculate all columns means per clusters
+        cluster_means = np.array(
+            [np.mean(np.array(cluster_citizens), axis=0) for cluster_citizens in clusters_citizens]
+        )
+        original_clusters = cluster_means.copy()
+        # Lowest weight
+        lowest_weight = cluster_means[:, 1].argmin()
+        planets["Venus"] = cluster_means[lowest_weight]
+        cluster_means = np.delete(cluster_means, lowest_weight, axis=0)
+        # Bone density + Height
+        biggest_height = cluster_means[:, 0].argmax()
+        lowest_bone = cluster_means[:, 2].argmin()
+        selected = None
+        if biggest_height == lowest_bone:
+            selected = biggest_height
+        else:
+            planets["Mars"] = cluster_means[biggest_height]
+            cluster_means = np.delete(cluster_means, biggest_height, axis=0)
+            selected = lowest_bone
+        planets["Belt"] = cluster_means[selected]
+        cluster_means = np.delete(cluster_means, selected, axis=0)
+        # Mars
+        if "Mars" not in planets:
+            biggest_height = cluster_means[:, 0].argmax()
+            planets["Mars"] = cluster_means[biggest_height]
+            cluster_means = np.delete(cluster_means, biggest_height, axis=0)
+        # Remaining is earth
+        planets["Earth"] = cluster_means[0]
+
+        # Map planets to an index
+        indexed_planets = []
+        for row in original_clusters:
+            for key, value in planets.items():
+                if np.array_equal(value, row):
+                    indexed_planets.append(key)
+                    break
+        return [citizens, indexed_planets]
 
 
 if __name__ == '__main__':
@@ -91,6 +138,7 @@ if __name__ == '__main__':
         print("Usage: Kmeans.py filepath='solar_system_census.csv' ncentroid=4 max_iter=30")
         exit()
     dataset_path, ncentroid, max_iter = sys.argv[1:]
+
     # Parse arguments
     try:
         ncentroid = int(ncentroid)
@@ -104,6 +152,7 @@ if __name__ == '__main__':
             raise ValueError("max_iter should be positive")
     except BaseException:
         print("Invalid max_iter `{}`".format(max_iter))
+
     # Open dataset
     dataset = False
     with CsvReader(dataset_path) as file:
@@ -114,27 +163,36 @@ if __name__ == '__main__':
     if dataset is False or len(dataset.shape) != 2 and dataset.shape[1] != 4:
         print('Invalid dataset')
         exit()
+
     # Remove "ID" column
     dataset = np.delete(dataset, 0, axis=1)
+
     # Fit and show results !
     kmeans = KmeansClustering(ncentroid=ncentroid, max_iter=max_iter)
     history = kmeans.fit(dataset)
-    print("history", history)
-    predictions = kmeans.predict(dataset)
+    [predictions, planets] = kmeans.predict(dataset)
+    print("predictions {} planets {}".format(predictions, planets))
+
     # Show the results
     fig = plt.figure()
     ax = plt.axes(projection='3d')
+    ax.set_xlabel("Height")
+    ax.set_ylabel("Weight")
+    ax.set_zlabel("Bone Density")
     cmap = plt.cm.get_cmap('tab20c', ncentroid)
     markers = ['+', ',', 'o', '^']
     for i, value in enumerate(dataset):
-        z, x, y = value
+        x, y, z = value
         ax.scatter3D(x, y, z, s=20, marker=markers[predictions[i] % 4], color=cmap(predictions[i]))
     for i in range(ncentroid):
         # Select the i-th row of the positions history
         line = history[0:, i]
-        z, x, y = line[:, 0], line[:, 1], line[:, 2]
+        x, y, z = line[:, 0], line[:, 1], line[:, 2]
         ax.plot3D(x, y, z, color=cmap(i))
-        last_z, last_x, last_y = z[-1], x[-1], y[-1]
-        print("({}) last is {}".format(i, last_z))
+        last_x, last_y, last_z = x[-1], y[-1],  z[-1]
+        # if ncentroid == 4:
+        #     ax.scatter3D(last_x, last_y, last_z, s=60, marker=markers[predictions[i] % 4], color=cmap(i), label=planets[predictions[i]])
+        # else:
         ax.scatter3D(last_x, last_y, last_z, s=60, marker=markers[predictions[i] % 4], color=cmap(i))
+    # plt.legend(fancybox=True, framealpha=1, shadow=True, borderpad=1, title='Planets')
     plt.show()
