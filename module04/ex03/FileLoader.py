43ed4ba409717759a56d3d43cdf30164c3833bfc
Glagan <8447621+Glagan@users.noreply.github.com>
2022-04-01 15:27:28 +0200

    Update FileLoader

--- module04/ex03/FileLoader.py
+++ module04/ex03/FileLoader.py
@@ -2,16 +2,23 @@ import pandas as pd
 
 
 class FileLoader:
-    def load(self, path):
+    def load(self, path: str):
+        if not isinstance(path, str):
+            return None
         try:
             df = pd.read_csv(path)
-            print('Loading dataset of dimensions {} x {}'.format(*df.shape))
+            print('Loading dataset of dimensions {}'.format(' x '.join(str(i) for i in df.shape)))
             return df
-        except IOError as err:
-            print('Failed to load {}: {}'.format(path, err))
+        except OSError as err:
+            print('Failed to load dataset: {}'.format(err))
+            return None
 
-    def display(self, df, n):
-        if n >= 0:
+    def display(self, df: pd.DataFrame, n: int):
+        if not isinstance(df, pd.DataFrame):
+            return
+        if not isinstance(n, int) or n == 0:
+            return
+        if n > 0:
             print(df.head(n))
         else:
             print(df.tail(-n))
